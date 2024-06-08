7fc8053147b76a121c47f9639c4a44e16bfd5aa8
Jobayer Hossain <jobayerhossain12021998@gmail.com>
2024-06-08 20:43:26 +0600

    m

--- app.js
+++ app.js
@@ -561,7 +561,7 @@ app.post('/api/admin-login', async (req, res) => {
 });
 
 // Start the server
-const port = 3000;
+const port = 3030;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
